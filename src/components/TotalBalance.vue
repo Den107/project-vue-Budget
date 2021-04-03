0a1b2c5fac86ecf1ca7fffb5999825446b55a089
Den107 <deniskrv@gmail.com>
2021-04-04 01:24:38 +0300

    add color balance

--- src/components/TotalBalance.vue
+++ src/components/TotalBalance.vue
@@ -1,24 +1,42 @@
 <template>
-  <div class="total-value">Balance: {{total}}</div>
+  <div class="total-value" :class="classObj">Balance: {{ total }}</div>
 </template>
 
 <script>
 export default {
-  name:'TotalBalance',
-  props:{
-    total:{
+  name: "TotalBalance",
+  props: {
+    total: {
       type: Number,
-      default: 0
-    }
-  }
-}
+      default: 0,
+    },
+  },
+  computed: {
+    classObj() {
+      return {
+        positive: this.total > 0,
+        negative: this.total < 0,
+        neutral: this.total === 0,
+      };
+    },
+  },
+};
 </script>
 
 <style scoped>
-.total-value{
+.total-value {
   font-size: 26px;
   text-transform: uppercase;
   padding: 20px;
   text-align: center;
+}
+.positive {
+  color: green;
+}
+.negative {
+  color: crimson;
+}
+.neutral {
+  color: black;
 }
 </style>
